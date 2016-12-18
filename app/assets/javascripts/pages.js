188ca4eadd8688fe4009d46d325f3ba9d402c245
Peter Jang <peter.x.jang@gmail.com>
2016-12-18 10:50:02 -0600

    Add example of writing API data to HTML

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,5 +1,18 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
-$.get('https://data.cityofchicago.org/resource/xzkq-xp2w.json', function(response) {
-  console.log(response);
-});
+function getData() {
+  var divEmployees = document.getElementById("employees");
+  divEmployees.innerHTML = "Loading...";
+  
+  $.get('https://data.cityofchicago.org/resource/xzkq-xp2w.json', function(employees) {
+    console.log(employees);
+    var htmlString = '';
+    for (var i = 0; i < employees.length; i++) {
+      var employee = employees[i];
+      htmlString += '<h2>' + employee["name"] + '</h2>';
+      htmlString += '<p>Department: ' + employee["department"] + '</p>';
+    }
+    divEmployees.innerHTML = htmlString;
+  });
+}
+

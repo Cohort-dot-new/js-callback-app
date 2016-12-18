83179fd2c41b88434cb19b42417d3d02b27a0293
Peter Jang <peter.x.jang@gmail.com>
2016-12-18 09:54:45 -0600

    Complete async example

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -5,6 +5,12 @@ function changeColors() {
     document.getElementById("first").style["color"] = "red";
     setTimeout(function() {
       document.getElementById("second").style["color"] = "blue";
+      setTimeout(function() {
+        document.getElementById("third").style["color"] = "green";
+        setTimeout(function() {
+          document.getElementById("fourth").style["color"] = "orange";
+        }, 2000);
+      }, 2000);
     }, 2000);
   }, 2000);
 }

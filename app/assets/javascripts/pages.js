4d0286586e13a87d820a0108ea4e198117acf9a3
Peter Jang <peter.x.jang@gmail.com>
2016-12-18 09:41:10 -0600

    Add async example

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,2 +1,10 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+function changeColors() {
+  setTimeout(function() {
+    document.getElementById("first").style["color"] = "red";
+    setTimeout(function() {
+      document.getElementById("second").style["color"] = "blue";
+    }, 2000);
+  }, 2000);
+}

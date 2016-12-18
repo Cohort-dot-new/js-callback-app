a05870243798717edb384c1b5778bd6cb708fd8e
Peter Jang <peter.x.jang@gmail.com>
2016-12-18 11:31:47 -0600

    Add example with toggle color dynamically

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -3,16 +3,25 @@
 function getData() {
   var divEmployees = document.getElementById("employees");
   divEmployees.innerHTML = "Loading...";
-  
+
   $.get('https://data.cityofchicago.org/resource/xzkq-xp2w.json', function(employees) {
     console.log(employees);
     var htmlString = '';
     for (var i = 0; i < employees.length; i++) {
       var employee = employees[i];
+      htmlString += '<div onclick="changeEmployee(this)">';
       htmlString += '<h2>' + employee["name"] + '</h2>';
       htmlString += '<p>Department: ' + employee["department"] + '</p>';
+      htmlString += '</div>';
     }
     divEmployees.innerHTML = htmlString;
   });
 }
 
+function changeEmployee(divEmployee) {
+  if (divEmployee.style["color"] === 'lightgray') {
+    divEmployee.style["color"] = 'black';
+  } else {
+    divEmployee.style["color"] = 'lightgray';
+  }
+}
